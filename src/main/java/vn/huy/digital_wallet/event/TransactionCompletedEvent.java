b4818efec82ebbc6bc84b70d0fe886d56d6d2c7c
Quang Huy <huyquang2k5@gmail.com>
2026-03-06 10:30:59 +0700

    "feat: Hoàn thành TransactionCompletedEvent, lá thư sẽ gửi đi"

---
+++ src/main/java/vn/huy/digital_wallet/event/TransactionCompletedEvent.java
@@ -0,0 +1,18 @@
+package vn.huy.digital_wallet.event;
+
+import vn.huy.digital_wallet.model.Transaction;
+import vn.huy.digital_wallet.model.User;
+
+
+public record TransactionCompletedEvent(
+        Transaction transaction, // giao dịch vừa hoàn thành
+        User actor,              // người thực hiện (người gửi)
+        String ipAddress,        // IP hấy từ HttpServletRequest
+        String userAgent         // Thiết bị lấy từ Header
+) {
+
+}
+
+/*
+Event là cái phong bì - nhét dữ liệu vào đây và gửi đi qua loa. Listener sẽ mở phong bì ra xài
+ */
