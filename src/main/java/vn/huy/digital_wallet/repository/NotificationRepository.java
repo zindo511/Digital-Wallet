e66867f46c41b9d03debffc14f82deefb1b84fda
Quang Huy <huyquang2k5@gmail.com>
2026-03-09 15:42:15 +0700

    "feat: hoàn thiện tính năng isRead Notification"

--- src/main/java/vn/huy/digital_wallet/repository/NotificationRepository.java
+++ src/main/java/vn/huy/digital_wallet/repository/NotificationRepository.java
@@ -1,7 +1,20 @@
 package vn.huy.digital_wallet.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import vn.huy.digital_wallet.model.Notification;
 
-public interface NotificationRepository extends JpaRepository<Notification,Long> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+  Page<Notification> findByUser_IdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+
+  long countByUser_IdAndIsReadFalse(Long userId);
+
+  @Modifying
+  @Query("UPDATE Notification n SET n.isRead = true WHERE n.user.id = :userId AND n.isRead = false")
+  void markAllAsReadByUserId(@Param("userId") Long userId);
 }
